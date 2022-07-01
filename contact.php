d50b3d6104e83f932d0676072d1f3724c8d47f54
Segaaloul <selim03gaaloul@gmail.com>
2022-07-01 17:32:41 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-$result = mail("selim03gaaloul@gmail.com","essai mail","coucou les amis","selim03gaaloul@gmail.com");
+$result = mail("selim03gaaloul@gmail.com","essai mail","coucou les amis","");
 if($result)
 {
     echo "<p> mail envoyer </p>" ;
