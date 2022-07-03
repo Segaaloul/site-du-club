90f8954e5b853bc2f19f0b85c78187c3de51721a
Segaaloul <selim03gaaloul@gmail.com>
2022-07-03 10:17:33 +0100

    barcha laab

--- propos.php
+++ propos.php
@@ -49,7 +49,7 @@
         <meta charset="UTF-8">
         <title>A Propos</title>
     
-        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <link rel="stylesheet" type="text/css" href="css/styles.css" media="screen" />
     </head>
     
     <body>
